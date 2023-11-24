3568523bd630b022628758fd7325150e0013cc14
prithvikr66 <pkunofficial66@gmail.com>
2023-11-24 09:26:54 +0530

    fixed some express errors

--- Server/index.js
+++ Server/index.js
@@ -4,26 +4,22 @@ const app = express();
 const PORT = 4500;
 
 app.get('/api/flag1',(req,res)=>{
-    res
-    .set({
+    res.set({
         "First-Part-of-the-flag": "FlAG_1_HERE",
          "Hint-For-Last-Part-Of-Flag": "focus on the api endpoint"
     })
-    .setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
-    .removeHeader("Etag")
-    .status(200)
-    .json({
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
+    res.removeHeader("Etag")
+    res.status(200).json({
         succes:true,
         message:"You are on the wrong side of things...you should be monitoring the network traffic of website"
     })
 })
 app.get('/api/flag2',(req,res)=>{
-    res
-       .header("Final-Part-Of-The-Flag", "flag2")
-       .setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
-       .removeHeader("Etag")
-       .status(200)
-       .json({
+    res.header("Final-Part-Of-The-Flag", "flag2")
+       res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
+       res.removeHeader("Etag")
+       res.status(200).json({
         success:true,
         message:"You are just one step away from getting your final part of the flag"
        })
