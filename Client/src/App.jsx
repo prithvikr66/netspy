0d4e4314517f5b8de9200587c9d98ca2adbbaece
prithvikr66 <pkunofficial66@gmail.com>
2023-11-24 10:15:08 +0530

    made some ui changes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
               <a href="" class="flex items-center space-x-3 rtl:space-x-reverse">
                 <TbSpy className=" text-4xl text-white"/>
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">NetSpy</span>
+                <span class="self-center text-2xl font-extrabold whitespace-nowrap dark:text-white">NetSpy</span>
               </a>
               <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                 <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
