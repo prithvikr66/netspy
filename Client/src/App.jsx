60133f56d981152aa9beb6b38e58420053b36961
prithvikr66 <pkunofficial66@gmail.com>
2023-12-04 12:55:27 +0530

    flags updated

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <>
       <div className=" bg-gray-900 h-screen ">
-
         <div >
           <nav class="bg-white border-gray-200 dark:bg-gray-900">
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
--- Server/index.js
+++ Server/index.js
@@ -2,11 +2,12 @@ import express from "express";
 import cors from "cors";
 const app = express();
 const PORT = 4500;
+// ritCTF{_1cy_penguin_j0y_}
 
 app.get('/api/flag/1',(req,res)=>{
     res.set({
-        "First-Part-of-the-flag": "FlAG_1_HERE",
-         "Hint-For-Last-Part-Of-Flag": "focus on the api endpoint"
+        "First-Part-of-the-flag": "ritCTF{_1cy_pen",
+         
     })
     res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
     res.removeHeader("Etag")
@@ -16,13 +17,14 @@ app.get('/api/flag/1',(req,res)=>{
     })
 })
 app.get('/api/flag/2',(req,res)=>{
-    res.header("Final-Part-Of-The-Flag", "flag2")
+    res.header("Final-Part-Of-The-Flag", "guin_j0y_}")
        res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
        res.removeHeader("Etag")
        res.status(200).json({
         success:true,
         message:"You are just one step away from getting your final part of the flag"
        })
+   
        
 });
 
