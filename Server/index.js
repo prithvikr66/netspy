cc03f328e9ae0fab55078eef0bf55ce032d1bc5a
prithvikr66 <pkunofficial66@gmail.com>
2023-12-07 23:19:04 +0530

    made the question easier

--- Server/index.js
+++ Server/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import cors from "cors";
 const app = express();
 const PORT = 4500;
-
+ 
+// ritCTF{_1cy_penguin_j0y_}
 
 app.get('/api/flag/1',(req,res)=>{
     res.set({
         "First-Part-of-the-flag": "ritCTF{_1cy_pen",
+        "Hint-for-the-final-part-of-the-flag": "Focus on the api endpoint url"
          
     })
     res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
@@ -17,9 +19,9 @@ app.get('/api/flag/1',(req,res)=>{
     })
 })
 app.get('/api/flag/2',(req,res)=>{
-       res.set({"Flag":"Not here this time"})
+       res.set({"Second-part-of-the-flag":"guin_j0y_}"})
        res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
-       res.cookie("Final-Part-of-the-Flag","guin_j0y_}",600)
+    //    res.cookie("Final-Part-of-the-Flag","guin_j0y_}",600)
        res.removeHeader("Etag")
        res.status(200).json({
         success:true,
